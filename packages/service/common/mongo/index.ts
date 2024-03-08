af1abf3fac0198baedf07334c0ffb9b17b331bad
baitaotu <baitaotu@pinduoduo.com>
2024-03-08 13:32:16 +0800

    base

--- packages/service/common/mongo/index.ts
+++ packages/service/common/mongo/index.ts
@@ -1,6 +1,8 @@
-import mongoose from 'mongoose';
-
-export default mongoose;
-export * from 'mongoose';
-
-export const connectionMongo = global.mongodb || mongoose;
+// import Database from 'better-sqlite3';
+import { Sequelize } from 'Sequelize';
+// export default Database;
+// const db = new Database('PrivateFastGPT.db', { verbose: console.log });
+// db.pragma('journal_mode = WAL');
+const sequelize = new Sequelize('sqlite::memory:');
+export const sqlite3 = sequelize;
+export * from 'Sequelize';
